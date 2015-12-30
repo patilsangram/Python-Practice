730b7a9fbe63d0b4a3160f9e3f3a98bc950115cf
patilsangram <sangram.p@indictranstech.com>
2015-12-30 15:08:20 +0530

    added changes in import files

--- Python/list_method.py
+++ Python/list_method.py
@@ -6,24 +6,30 @@ for ele in range(len):
 	list.append(input)
 print list
 
-method=int(raw_input(" 1.Add \n 2.Remove \n 3.Reverse \n 4.Display \n 5.Delete \n"))
+while True:
+	method=int(raw_input(" 1.Add \n 2.Remove \n 3.Reverse \n 4.Display \n 5.Delete \n 6.Exit"))
 
-if method==1:
-	add=raw_input("Enter to add : \n")
-	list.append(add)
-	print"list after adding...",list,"\n"
-elif method==2:
-	remove_ele=raw_input("Remove element : \n")
-	list.remove(remove_ele)
-	print"After remove \n",list
-elif method==3:
-	list.reverse()
-	print list
-elif method==4:
-	print "Your List ", list
-elif method==5:
-	del_ele=int(raw_input("Enter index of del ele : \n"))
-	del list[del_ele]
-	print list
-else:
-	print"Enter valid choice :\n"
+	if method==1:
+		add=raw_input("Enter to add : \n")
+		list.append(add)
+		print"list after adding...",list,"\n"
+	elif method==2:
+		remove_ele=raw_input("Remove element : \n")
+		list.remove(remove_ele)
+		print"After remove \n",list
+	elif method==3:
+		list.reverse()
+		print list
+	elif method==4:
+		print "Your List ", list
+	elif method==5:
+		del_ele=int(raw_input("Enter index of del ele : \n"))
+		if del_ele in list:
+			del list[del_ele]
+			print list
+		else:
+			print "element not present"
+	elif method ==6:
+		exit()
+	else:
+		print"Enter valid choice :\n"
--- import_read_write_file.py
+++ import_read_write_file.py
@@ -2,13 +2,34 @@ from sys import argv
 
 script, filename = argv
 
-print "file is: %s"%filename
+print "file is: '%s'"%filename
 txt = open(filename)
+print'"current file"'
 print txt.read()
-clear_file = open(filename, 'w')
-clear_file.truncate()
 
-write_file = open(filename, 'rw+')
+print "'now we clear the file'"
+txt = open(filename, 'w')
+txt.truncate()
 
-write_file.write("hello")
-write_file.read()
+print "'add lines to file'"
+line1 = raw_input("Line1 :")
+line2 = raw_input("Line2 :")
+line3 = raw_input("Line3 :")
+
+"""you can write multiple lines once"""
+txt.writelines([line1, "\n", line2, "\n", line3, "\n"])
+"""OR like this one : 
+txt.write("%s \n %s \n %s \n" % (line1, line2, line3))"""
+
+"""Or you can write single lines"""
+txt.write("\n")
+txt.write(line2)
+txt.write("\n")
+txt.write(line3)
+txt.write("\n")
+
+print '"read final file"'
+txt = open(filename, 'r')
+print txt.read()
+txt.close()
+
